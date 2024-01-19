e0a2b983b5c4c09744d0263d8e0b024edb18a8fb
Levi Powis <lpowis97@gmail.com>
2024-01-19 10:20:43 -0500

    added html for submit button and added an event listener that runs when the submit button is clicked

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 import "./style.css";
+
+const tipCalculatorForm = document.getElementById("tipCalculatorForm");
+
+tipCalculatorForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let billAmount = parseFloat(document.getElementById("billAmount").value);
+  let tipPercentage = parseFloat(document.querySelector("input[type = radio]:checked").value);
+  let tipAmount = tipPercentage * billAmount;
+  let totalAmount = billAmount + tipAmount;
+  let USDollar = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
+
+  document.getElementById("tipAmountOutput").innerHTML = "Tip Amount: " + USDollar.format(tipAmount);
+  document.getElementById("totalAmountOutput").innerHTML = "Total Amount: " + USDollar.format(totalAmount);
+});
