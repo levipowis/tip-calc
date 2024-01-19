c678965401abdeb065e0c22203be3e2864e5188c
Levi Powis <lpowis97@gmail.com>
2024-01-19 13:20:40 -0500

    added comments to main.js and refactored to improve readability

--- main.js
+++ main.js
@@ -4,16 +4,32 @@ const tipCalculatorForm = document.getElementById("tipCalculatorForm");
 
 tipCalculatorForm.addEventListener("submit", (e) => {
   e.preventDefault();
+  // Input data from billAmount and tipPercentage fields
+  let billAmountInput = document.getElementById("billAmountInput").value;
+  let tipPercentageInput = document.querySelector("input[type = radio]:checked").value;
 
-  let billAmount = parseFloat(document.getElementById("billAmountInput").value);
-  let tipPercentage = parseFloat(document.querySelector("input[type = radio]:checked").value);
+  // Convert from strings to floats
+  let billAmount = parseFloat(billAmountInput);
+  let tipPercentage = parseFloat(tipPercentageInput);
+
+  // tipAmount and totalAmount calculations
   let tipAmount = tipPercentage * billAmount;
   let totalAmount = billAmount + tipAmount;
+
+  // Gets divs to output tipAmount and totalAmount
+  let tipAmountOutput = document.getElementById("tipAmountOutput");
+  let totalAmountOutput = document.getElementById("totalAmountOutput");
+
+  // Constructor used to format currency
   let USDollar = new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
   });
 
-  document.getElementById("tipAmountOutput").innerHTML = "Tip Amount: " + USDollar.format(tipAmount);
-  document.getElementById("totalAmountOutput").innerHTML = "Total Amount: " + USDollar.format(totalAmount);
+  if (billAmountInput === "") {
+    tipAmountOutput.innerHTML = "Please Enter a Bill Amount";
+  } else {
+    tipAmountOutput.innerHTML = "Tip Amount: " + USDollar.format(tipAmount);
+    totalAmountOutput.innerHTML = "Total Amount: " + USDollar.format(totalAmount);
+  }
 });
